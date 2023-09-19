4c35e3c90fc3043fd9b17fa8b986e932b14bc9ea
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-09-19 08:16:04 +0100

    add checks for gin mode

--- api/routes.go
+++ api/routes.go
@@ -4,10 +4,15 @@ import (
 	"demerzel-events/internal/handlers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func BuildRoutesHandler() *gin.Engine {
 	r := gin.New()
+
+	if os.Getenv("APP_ENV") == "prod" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
