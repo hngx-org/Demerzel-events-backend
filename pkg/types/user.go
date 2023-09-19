9603730415a2c93a6a29b5842f3530cc61bc4fa6
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-09-20 00:02:18 +0100

    add openid and profile options to oauth

--- pkg/types/user.go
+++ pkg/types/user.go
@@ -1,9 +1,11 @@
 package types
 
 type UserInfo struct {
-	Sub     string `json:"sub"`
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	Picture string `json:"picture"`
-	Locale  string `json:"locale"`
+	Sub        string `json:"sub"`
+	Name       string `json:"name"`
+	GivenName  string `json:"given_name"`
+	FamilyName string `json:"family_name"`
+	Email      string `json:"email"`
+	Picture    string `json:"picture"`
+	Locale     string `json:"locale"`
 }
