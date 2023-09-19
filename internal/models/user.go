7f133dc1a87013945e15fa0c60c1b2a0a4599feb
john <jenweani@gmail.com>
2023-09-19 12:00:31 +0100

    added user models and user service

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,5 +1,43 @@
 package models
 
 type User struct {
-	// Add user fields
+	UuId        int `json:"uuid"`
+	Name string `json:"name" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required"`
+}
+
+type UserResponse struct {
+	Username string
+	Email string
+	Token string
+	TokenType string
+}
+
+type UserSignUp struct {
+	Username string `json:"username" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type UpdateUser struct {
+	Email           string `json:"email" validate:"email"`
+	CurrentPassword string `json:"current_password"`
+	NewPassword     string `json:"new_password"`
+	ConfirmPassword string `json:"confirm_password"`
+}
+
+type PasswordReset struct {
+	Email string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	ConfirmPassword string `json:"confirm_password" validate:"required"`
+}
+
+type ForgotPassword struct {
+	Email string `json:"email" validate:"required,email"`
 }
