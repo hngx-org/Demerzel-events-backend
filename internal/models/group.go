326271d6a98b26c6ff9daa2864d9a09c94e377f4
badiru <badirusulaimon27@gmail.com>
2023-09-19 11:03:35 +0100

    added uuid

--- internal/models/group.go
+++ internal/models/group.go
@@ -1,6 +1,8 @@
 package models
 
- type Group struct {
-	Id string `json:"id,omitempty"`
-	Name string `json:"name,omitempty"`
- }
+import "github.com/google/uuid"
+
+type Group struct {
+	ID   uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+	Name string    `json:"name" validate:"required"`
+}
