eb8c22a6dfd74ff16a5ce80898505e4022851d1c
JerryAgbesi <jerryeagbesi@gmail.com>
2023-09-20 16:17:36 +0000

    Add route for retrieving a specific event

--- api/routes.go
+++ api/routes.go
@@ -21,6 +21,7 @@ func BuildRoutesHandler() *gin.Engine {
 
 	r.GET("/health", handlers.HealthHandler)
 	r.POST("/events", handlers.CreateEventHandler)
+	r.GET("/events/:id",handlers.GetEventHandler)
 
 	return r
 }
