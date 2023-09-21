98dbf919ab75b6de331d19727034eae6658cfeb9
Oreva Eniworo <eniworoeva@yahoo.com>
2023-09-21 14:46:24 +0100

    refactored route

--- internal/routes/comment-routes.go
+++ internal/routes/comment-routes.go
@@ -10,5 +10,5 @@ func CommentRoutes(router *gin.RouterGroup) {
 	// Add routes and handler you are working on here
 	// handler from Demerzel-events-backend/internal/handlers/comment-handler.go file
 	router.POST("/events/:event_id/comments", handlers.CreateComment)
-	router.PUT("/events/:event_id/comments/:comment_id", handlers.UpdateComment)
+	router.PUT("/events/comments/:comment_id", handlers.UpdateComment)
 }
