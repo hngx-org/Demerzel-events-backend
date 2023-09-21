9cc9cefa61b8728e8b6bca6e3dc9c3ae2c1445b9
atanda0x <atandanafiu21@gmail.com>
2023-09-21 02:39:58 -0700

    refactored creategroup and UpdateGroup

--- internal/handlers/group.go
+++ internal/handlers/group.go
@@ -5,29 +5,10 @@ import (
 	"demerzel-events/internal/models"
 	"demerzel-events/pkg/response"
 	"demerzel-events/services"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
-
-func CreateGroup(ctx *gin.Context) {
-	var requestBody struct {
-		Name string `json:"name" binding:"required"`
-	}
-
-	if err := ctx.ShouldBindJSON(&requestBody); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
-		return
-	}
-
-	var newGroup models.Group
-	newGroup.Name = requestBody.Name
-
-	services.CreateGroup(&newGroup)
-
-	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "message": "", "data": newGroup})
-}
 
 func UnsubscribeFromGroup(c *gin.Context) {
 	groupID := c.Param("id")
@@ -59,24 +40,42 @@ func UnsubscribeFromGroup(c *gin.Context) {
 	response.Success(c, "User successfully unsubscribed to group", map[string]any{})
 
 }
+func CreateGroup(ctx *gin.Context) {
+	var requestBody struct {
+		Name string `json:"name" binding:"required"`
+	}
+
+	if err := ctx.ShouldBindJSON(&requestBody); err != nil {
+		response.Error(ctx, err.Error())
+		return
+	}
+
+	var newGroup models.Group
+	newGroup.Name = requestBody.Name
+
+	_, err := services.CreateGroup(&newGroup)
+	if err != nil {
+		response.Error(ctx, err.Error())
+		return
+	}
+
+	response.Success(ctx, "Group Created Successfully", newGroup)
+}
+
 func UpdateGroup(c *gin.Context) {
 	req := models.UpdateGroupRequest{}
 	id := c.Params.ByName("id")
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"status": http.StatusBadRequest, "message": "failed to parse request", "error": err.Error()})
+		response.Error(c, err.Error())
 		return
 	}
 
-	code, data, err := services.UpdateGroupService(db.DB, req, id)
+	_, data, err := services.UpdateGroupService(db.DB, req, id)
 	if err != nil {
-		c.JSON(code, gin.H{"status": code, "message": "failed to parse request", "error": err.Error()})
+		response.Error(c, err.Error())
 		return
 	}
 
-	c.JSON(code, gin.H{
-		"status":  "success",
-		"message": "Group Name updated successfully",
-		"data":    data,
-	})
+	response.Success(c, "Group Name updated successfully", data)
 }
