df6c6cefc934ebc306bb246c55935c0cc375aef9
0xAckerMan <joelkores1@gmail.com>
2023-09-21 23:08:44 +0300

    [chore] Created the delete a comment endpoint route

--- internal/routes/comment-routes.go
+++ internal/routes/comment-routes.go
@@ -10,4 +10,5 @@ func CommentRoutes(router *gin.Engine) {
 	// Add routes and handler you are working on here
 	// handler from Demerzel-events-backend/internal/handlers/comment-handler.go file
 	router.POST("/events/:event_id/comments", handlers.CreateComment)
+	router.DELETE("/events/:event_id/comments/:comment_id", handlers.DeleteComment)
 }
